2c1290760148691b171c41d6267384522843dbf3
Toby Clarke <teeohbee@gmail.com>
2015-11-11 22:06:46 +0000

    refactored some methods

--- rps_csharp/RPS.cs
+++ rps_csharp/RPS.cs
@@ -2,24 +2,43 @@
 
 public class RPS
 {
+	static Random rnd = new Random ();
+
 	public static string OnePlayer(string playerOneWeapon)
 	{
-		Random rnd = new Random ();
-		int opponentWeapon = rnd.Next (2);
-		return $"You chose {playerOneWeapon}";
+		string playerTwoWeapon = CPUWeapon();
+		return Play (playerOneWeapon, playerTwoWeapon);
 	}
+
+	static string CPUWeapon()
+	{
+		string[] weapons = { "Rock", "Paper", "Scissors" };
+		return weapons [rnd.Next (2)];
+	}
+
 
 	public static string TwoPlayer(string playerOneWeapon, string playerTwoWeapon)
 	{
+		return Play (playerOneWeapon, playerTwoWeapon);
+
+	}
+
+	static string Play (string playerOneWeapon, string playerTwoWeapon)
+	{
 		if (playerOneWeapon == playerTwoWeapon) {
 			return "It's a draw";
-		} else if (playerOneWeapon == "Rock") {
-			return PlayerOneRock (playerTwoWeapon);
-		} else if (playerOneWeapon == "Paper") {
-			return PlayerOnePaper (playerTwoWeapon);
-		} else {
-			return PlayerOneScissors (playerTwoWeapon);
 		}
+		else
+			if (playerOneWeapon == "Rock") {
+				return PlayerOneRock (playerTwoWeapon);
+			}
+			else
+				if (playerOneWeapon == "Paper") {
+					return PlayerOnePaper (playerTwoWeapon);
+				}
+				else {
+					return PlayerOneScissors (playerTwoWeapon);
+				}
 	}
 
 	static string PlayerOneRock (string playerTwoWeapon)
@@ -30,7 +49,6 @@ public class RPS
 	static string PlayerOnePaper (string playerTwoWeapon)
 	{
 		return (playerTwoWeapon == "Rock") ? "Player One Wins" : "Player Two Wins";
-
 	}
 
 	static string PlayerOneScissors (string playerTwoWeapon)
