7ecd34e54f53d0e8624bcebbd4b8da2a4822fc03
Toby Clarke <teeohbee@gmail.com>
2015-11-11 18:44:48 +0000

    Added first tests

---
+++ rps_csharp/RPS.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class RPS
+{
+	public static string OnePlayer(string playerOneWeapon)
+	{
+		Random rnd = new Random ();
+		int opponentWeapon = rnd.Next (2);
+		return $"You chose {playerOneWeapon}";
+	}
+
+	public static string TwoPlayer(string playerOneWeapon, string playerTwoWeapon)
+	{
+		return "Player One Wins";
+	}
+}
---
+++ rps_csharp/RPSTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+[TestFixture]
+public class RPSTests
+{
+	[TestCase("Rock","Paper","Player One Wins")]
+	[TestCase("Paper","Rock","Player Two Wins")]
+	public void PlayerVsPlayer(string playerOneWeapon, string playerTwoWeapon, string expected)
+	{
+		string result = RPS.TwoPlayer (playerOneWeapon, playerTwoWeapon);
+		Assert.That (result, Is.EqualTo (expected));
+	}
+
+	[TestCase("Rock","You chose Rock")]
+	[TestCase("Paper","You chose Paper")]
+	[TestCase("Scissors","You chose Scissors")]
+	public void PlayerVsComputer(string playerOneWeapon, string expected)
+	{
+		string result = RPS.OnePlayer (playerOneWeapon);
+		Assert.That (result, Is.EqualTo (expected));
+	}
+
+}
