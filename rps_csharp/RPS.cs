5fd4472fb8aa236f36b377a5784917e2526b6ff1
Toby Clarke <teeohbee@gmail.com>
2015-11-11 19:28:05 +0000

    Player vs player complete

--- rps_csharp/RPS.cs
+++ rps_csharp/RPS.cs
@@ -11,6 +11,44 @@ public class RPS
 
 	public static string TwoPlayer(string playerOneWeapon, string playerTwoWeapon)
 	{
-		return "Player One Wins";
+		if (playerOneWeapon == playerTwoWeapon) {
+			return "It's a draw";
+		} else if (playerOneWeapon == "Rock") {
+			return PlayerOneRock (playerTwoWeapon);
+		} else if (playerOneWeapon == "Paper") {
+			return PlayerOnePaper (playerTwoWeapon);
+		} else {
+			return PlayerOneScissors (playerTwoWeapon);
+		}
+	}
+
+	static string PlayerOneRock (string playerTwoWeapon)
+	{
+		if (playerTwoWeapon == "Scissors") {
+			return "Player One Wins";
+		}
+		else {
+			return "Player Two Wins";
+		}
+	}
+
+	static string PlayerOnePaper (string playerTwoWeapon)
+	{
+		if (playerTwoWeapon == "Rock") {
+			return "Player One Wins";
+		}
+		else {
+			return "Player Two Wins";
+		}
+	}
+
+	static string PlayerOneScissors (string playerTwoWeapon)
+	{
+		if (playerTwoWeapon == "Paper") {
+			return "Player One Wins";
+		}
+		else {
+			return "Player Two Wins";
+		}
 	}
 }
--- rps_csharp/RPSTest.cs
+++ rps_csharp/RPSTest.cs
@@ -3,8 +3,15 @@
 [TestFixture]
 public class RPSTests
 {
-	[TestCase("Rock","Paper","Player One Wins")]
-	[TestCase("Paper","Rock","Player Two Wins")]
+	[TestCase("Rock","Scissors","Player One Wins")]
+	[TestCase("Rock","Rock","It's a draw")]
+	[TestCase("Rock","Paper","Player Two Wins")]
+	[TestCase("Paper","Rock","Player One Wins")]
+	[TestCase("Paper","Paper","It's a draw")]
+	[TestCase("Paper","Scissors","Player Two Wins")]
+	[TestCase("Scissors","Paper","Player One Wins")]
+	[TestCase("Scissors","Scissors","It's a draw")]
+	[TestCase("Scissors","Rock","Player Two Wins")]
 	public void PlayerVsPlayer(string playerOneWeapon, string playerTwoWeapon, string expected)
 	{
 		string result = RPS.TwoPlayer (playerOneWeapon, playerTwoWeapon);
